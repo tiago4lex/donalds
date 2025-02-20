a768dc2c43ac5246862adda494edae5e834387fe
Tiago <tiago.4lexandre@gmail.com>
2025-02-20 16:42:24 -0300

    feat: add restaurant menu page with categories and products

---
+++ src/app/[slug]/menu/components/products.tsx
@@ -0,0 +1,47 @@
+import { Product } from "@prisma/client";
+import Image from "next/image";
+import Link from "next/link";
+
+interface ProductsProps {
+  products: Product[];
+}
+
+const Products = ({ products }: ProductsProps) => {
+  return (
+    <div className="space-y-3 px-5">
+      {products.map((product) => (
+        <Link
+          key={product.id}
+          className="flex items-center justify-between gap-10 py-3 border-b"
+          href="/"
+        >
+          {/* ESQUERDA */}
+          <div>
+            <h3 className="text-sm font-medium">{product.name}</h3>
+            <p className="line-clamp-2 text-sm text-muted-foreground">
+              {product.description}
+            </p>
+            <p className="pt-3 text-sm font-semibold">
+              {new Intl.NumberFormat("pt-BR", {
+                style: "currency",
+                currency: "BRL",
+              }).format(product.price)}
+            </p>
+          </div>
+
+          {/* DIREITA */}
+          <div className="relative min-h-[82px] min-w-[120px]">
+            <Image
+              src={product.imageUrl}
+              fill
+              alt={product.name}
+              className="rounded-lg object-contain"
+            />
+          </div>
+        </Link>
+      ))}
+    </div>
+  );
+};
+
+export default Products;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 const HomePage = () => {
-  return ( <h1>Hello World!</h1> );
-}
- 
+  return <h1>Hello World!</h1>;
+};
+
 export default HomePage;
