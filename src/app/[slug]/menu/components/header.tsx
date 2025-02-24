9f7c4b1d1911a41c9aee6874d95c7ab1b131876a
Tiago <tiago.4lexandre@gmail.com>
2025-02-23 21:42:20 -0300

    feat: ensure all orders and back buttons work as expected

--- src/app/[slug]/menu/components/header.tsx
+++ src/app/[slug]/menu/components/header.tsx
@@ -3,7 +3,7 @@
 import { Restaurant } from "@prisma/client";
 import { ChevronLeftIcon, ScrollTextIcon } from "lucide-react";
 import Image from "next/image";
-import { useRouter } from "next/navigation";
+import { useParams, useRouter } from "next/navigation";
 
 import { Button } from "@/components/ui/button";
 
@@ -12,8 +12,10 @@ interface RestaurantHeaderProps {
 }
 
 const RestaurantHeader = ({ restaurant }: RestaurantHeaderProps) => {
+  const { slug } = useParams<{ slug: string }>();
   const router = useRouter();
   const handleBackClick = () => router.back();
+  const handleOrdersClick = () => router.push(`/${slug}/orders`);
   return (
     <div className="relative h-[250px] w-full">
       <Button
@@ -25,20 +27,21 @@ const RestaurantHeader = ({ restaurant }: RestaurantHeaderProps) => {
         <ChevronLeftIcon />
       </Button>
 
-      <Button
-        variant="secondary"
-        size="icon"
-        className="absolute right-4 top-4 z-50 rounded-full"
-      >
-        <ScrollTextIcon />
-      </Button>
-
       <Image
         src={restaurant.coverImageUrl}
         alt={restaurant.name}
         fill
         className="object-cover"
       />
+
+      <Button
+        variant="secondary"
+        size="icon"
+        className="absolute right-4 top-4 z-50 rounded-full"
+        onClick={handleOrdersClick}
+      >
+        <ScrollTextIcon />
+      </Button>
     </div>
   );
 };
--- src/app/[slug]/orders/components/cpf-form.tsx
+++ src/app/[slug]/orders/components/cpf-form.tsx
@@ -40,7 +40,7 @@ const CpfForm = () => {
     const router = useRouter();
     const pathname = usePathname();
     const onSubmit = (data: FormSchema) => {
-        router.push(`${pathname}?cpf=${removeCpfPunctuation(data.cpf)}`);
+        router.replace(`${pathname}?cpf=${removeCpfPunctuation(data.cpf)}`);
     }
     const handleCancel = () => {
         router.back();
